e8c2cca7436e2b63e98c78588324cbad5b84cf86
EnteNico <84995446+EnteNico@users.noreply.github.com>
2024-04-18 10:31:35 +0200

    Initial commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://github.com/ByteScripts/documentation',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  outDir: './out',
+};
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,75 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+import Search from '@components/Search';
 
-const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+function useHead() {
+  const { asPath } = useRouter();
+  const { frontMatter, title } = useConfig();
+  const url = `https://github.com/ByteScripts/documentation${asPath}`;
+  const description = frontMatter.description || "Documentation for ByteScripts's resources for FiveM";
+
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon" href="/static/logo.ico" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={url} />
+    </>
+  );
 }
 
-export default config
+function useNextSeoProps() {
+  const { asPath } = useRouter();
+  const arr = asPath.replace(/[-_]/g, ' ').split('/');
+  const category = (arr[1][0] !== '#' && arr[1]) || 'ByteScripts';
+  const rawTitle = arr[arr.length - 1];
+  const title = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : '%s';
+
+  return {
+    titleTemplate: `${title} - ${
+      rawTitle === category ? 'Documentation' : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
+    }`,
+  };
+}
+
+const config: DocsThemeConfig = {
+  logo: (
+    <div
+      style={{
+        paddingLeft: '50px',
+        lineHeight: '38px',
+        background: "url('https://avatars.githubusercontent.com/u/150040163?s=38') no-repeat left",
+        backgroundSize: '38px',
+        fontWeight: 550,
+      }}
+    >
+      ByteScripts
+    </div>
+  ),
+  project: {
+    link: 'https://github.com/ByteScripts/documentation',
+  },
+  chat: {
+    link: 'https://discord.gg/QVRWMtE2aM',
+  },
+  docsRepositoryBase: 'https://github.com/ByteScripts/documentation/blob/main',
+  footer: {
+    text: 'ByteScripts',
+  },
+  // search: {
+  //   component: <Search />,
+  // },
+  head: useHead,
+  primaryHue: { dark: 200, light: 200 },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+
+  useNextSeoProps: useNextSeoProps,
+};
+
+export default config;
